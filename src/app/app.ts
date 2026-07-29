d57bb27eca7a114cf041da6e9cbf04c9bc53083d
Beatriz Felix <beatriz-felix@pop-os.localdomain>
2026-07-29 12:28:09 -0300

    feat: create task component with sample content

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Task } from './task/task';
+import { TaskProps } from './task';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Task],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('my-to-do-list');
+  protected readonly title = 'my-to-do-list';
+
+  exampleTask: TaskProps = {
+    id: 1,
+    title: "Example task title",
+    description: "Example task description",
+    completed: false
+  }
 }
---
+++ src/app/task/task.ts
@@ -0,0 +1,12 @@
+import { Component, input } from '@angular/core';
+import { TaskProps } from '../task';
+
+@Component({
+  selector: 'app-task',
+  imports: [],
+  templateUrl: './task.html',
+  styleUrl: './task.scss',
+})
+export class Task {
+  task = input.required<TaskProps>();
+}
