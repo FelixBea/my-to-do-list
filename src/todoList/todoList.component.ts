1420a283c10fd8088df8b768eea393e8678c9740
Beatriz Felix <beatriz-felix@pop-os.localdomain>
2026-07-30 11:36:48 -0300

    feat: task form layout, styles

--- src/todoList/todoList.component.ts
+++ src/todoList/todoList.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TaskList } from './taskList/taskList.component';
+import { TaskForm } from "./task-form/task-form";
 
 @Component({
   selector: 'app-todo-list',
-  imports: [TaskList],
+  imports: [TaskList, TaskForm],
   templateUrl: './todoList.html',
   styleUrl: './todoList.scss',
 })
